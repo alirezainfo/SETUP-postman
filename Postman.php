123c7b5a05edfd1a8ff6c4909894e52390b78b03
Alireza sadeghi <147473038+alirezainfo@users.noreply.github.com>
2023-11-13 11:46:58 +0330

    Add files via upload

---
+++ Postman.php
@@ -0,0 +1,56 @@
+<!-- json ویرایش حساب کاربری و برگرداندن آن با  -->
+[
+    {
+        "admin_id": "1",
+        "adminname": "mohammadsadeghi",
+        "password": "1234",
+        "firstname": "mohammad",
+        "lastname": "sadeghi",
+        "phone": "91226565",
+        "email": "mohammad@gmail.com"
+    }
+]
+<!-- json حذف محصول برگرداندن آن با  -->
+[
+    {
+        "product_id": "2",
+        "name": "لوله سبز",
+        "warranty": "2025-11-19"
+    }
+]
+<!-- json دریافت اطلاعات همه ی کاربران -->
+[
+    {
+        "user_id": "1",
+        "username": "alireza1234",
+        "password": "12345678",
+        "firstname": "alireza",
+        "lastname": "sadeghi",
+        "phone": "09223434",
+        "address": "خیابان شهیدان شرقی",
+        "email": "alireza@gmail.com"
+    }
+    {
+        "user_id": "2",
+        "username": "hamid1234",
+        "password": "12345",
+        "firstname": "hamid",
+        "lastname": "sadeghi",
+        "phone": "09131179421",
+        "address": "خیابان شهیدان غربی",
+        "email": "hamid@gmail.com"
+    }
+]
+<!-- json دریافت اطلاعات کاربر شماره 2  -->
+[
+    {
+        "user_id": "2",
+        "username": "hamid1234",
+        "password": "12345",
+        "firstname": "hamid",
+        "lastname": "sadeghi",
+        "phone": "09131179421",
+        "address": "خیابان شهیدان غربی",
+        "email": "hamid@gmail.com"
+    }
+]
